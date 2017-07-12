4a998161c08391da7c317511592a1e8418d5eb1f
ThePandaJedi <matthew.haahr@gmail.com>
2017-07-12 09:49:34 -0700

    Update DMX_RGB_LED.ino

--- DMX_RGB_LED.ino
+++ DMX_RGB_LED.ino
@@ -4,16 +4,16 @@
 
 void setup() 
 {
-  ArduinoDmx0.set_control_pin(11); 
+  ArduinoDmx0.set_control_pin(11); //Defines control pin
   ArduinoDmx0.set_rx_address(1); 
-  ArduinoDmx0.set_rx_channels(192);
+  ArduinoDmx0.set_rx_channels(192); //Defines number of channels in a packet
   ArduinoDmx0.init_rx(DMX512);    
 }
 
 void loop()
 {
-  analogWrite(3, ArduinoDmx0.RxBuffer[0]);
-  analogWrite(5, ArduinoDmx0.RxBuffer[1]);
-  analogWrite(6, ArduinoDmx0.RxBuffer[2]);
+  analogWrite(3, ArduinoDmx0.RxBuffer[0]); //Red
+  analogWrite(5, ArduinoDmx0.RxBuffer[1]); //Green
+  analogWrite(6, ArduinoDmx0.RxBuffer[2]); //Blue
 } 
 
